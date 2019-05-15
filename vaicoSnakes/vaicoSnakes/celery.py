251672495ec5bb9270e969bce03f50aa29b18092
TheBaxes <sebas1475@gmail.com>
2019-05-15 13:36:02 -0500

    fix2

--- vaicoSnakes/vaicoSnakes/celery.py
+++ vaicoSnakes/vaicoSnakes/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings  # noqa
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vaicoSnakes.settings')
 
-app = Celery('vaicoSnakes', broker='amqp://guest@localhost')
+app = Celery('vaicoSnakes', broker='amqp://guest@rabbitmq')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
