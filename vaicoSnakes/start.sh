95d11b2bb762b20bf1817f263c5ee9b97ef17a7c
Diego Cifuentes <dcifuen3@gmail.com>
2019-05-19 23:29:06 +0000

    nginx and gunicorn

--- vaicoSnakes/start.sh
+++ vaicoSnakes/start.sh
@@ -4,19 +4,17 @@ echo Starting Gunicorn.
 #make sure we are at project root /usr/src/app
 cd $PROJECT_ROOT
 
-
 echo "Collecting and compiling statics"
-pushd vaicoSnakes
 python manage.py collectstatic --noinput
 echo "Making migrations"
 python manage.py makemigrations
 echo "Migrating"
 python manage.py migrate
-popd
 
 # Difference from article
 # CD to project as per comment above
-cd vaicoSnakes
 exec gunicorn vaicoSnakes.wsgi:application \
      --bind 0.0.0.0:8000 \
-         --workers 3
+         --workers 3 \
+    --timeout 120 \
+    --log-level=debug
